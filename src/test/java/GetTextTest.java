1169222099979113961bcd1344fced3956000791
sousFlex <memph1zgray@gmail.com>
2025-03-05 22:09:21 +0300

    Домашнее задание 4

---
+++ src/test/java/GetTextTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class GetTextTest {
+    @Test
+    public void testGetText(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
