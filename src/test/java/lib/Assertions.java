4402c2142f618298b5efc1c07f1707e49e40bbce
sousFlex <memph1zgray@gmail.com>
2025-04-01 20:33:16 +0300

    Добавлен DataGenerator, расширены ассерты, добавлены тесты на создание, редактирование, получение данных юзера

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -3,6 +3,7 @@ package lib;
 import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -13,4 +14,34 @@ public class Assertions {
         int value = response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "Actual value doesn't match expected value");
     }
+
+    public static void assertJsonByName(Response response, String name, String expectedValue) {
+        response.then().assertThat().body("$",hasKey(name));
+
+        String value = response.jsonPath().getString(name);
+        assertEquals(expectedValue, value, "Actual value doesn't match expected value");
+    }
+
+    public static void assertResponseTextEquals (Response response, String expectedAnswer){
+        assertEquals(expectedAnswer,response.asString(),"Response text is not as expected");
+    }
+
+    public static void assertResponseCodeEquals (Response response, int expectedStatusCode){
+        assertEquals(expectedStatusCode,response.statusCode(),"Response code is not as expected");
+    }
+
+    public static void assertJsonHasField(Response response, String expectedFieldName){
+        response.then().assertThat().body("$", hasKey(expectedFieldName));
+    }
+
+    public static void assertJsonHasFields(Response response, String[] expectedFieldNames){
+        for (String expectedFieldName : expectedFieldNames){
+            Assertions.assertJsonHasField(response,expectedFieldName);
+        }
+    }
+
+    public static void assertJsonHasNoField(Response response, String unexpectedFiledName){
+        response.then().assertThat().body("$", not(hasKey(unexpectedFiledName)));
+    }
+
 }
